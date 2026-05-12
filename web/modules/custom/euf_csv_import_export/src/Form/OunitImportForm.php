bdfd6dcf22409b06180c1f550f7deec18a15ff30
ittisvagyok <ittisvagyok@gmail.com>
2026-05-13 01:39:48 +0200

    Adds Ounit form

--- web/modules/custom/euf_csv_import_export/src/Form/OunitImportForm.php
+++ web/modules/custom/euf_csv_import_export/src/Form/OunitImportForm.php
@@ -3,11 +3,12 @@
 
 namespace Drupal\euf_csv_import_export\Form;
 
+use Drupal\euf_csv_import_export\Enum\ImportTargetEntityType;
 use Drupal\euf_csv_import_export\Form\ImportFormBase;
 
 class OunitImportForm extends ImportFormBase {
 
-	public const ENTITY_TYPE = 'ounit';
+	public const ENTITY_TYPE = ImportTargetEntityType::OUNIT->value;
   public const ENTITY_LABEL = 'Organizational Units';
 
   public function getFormId() {
