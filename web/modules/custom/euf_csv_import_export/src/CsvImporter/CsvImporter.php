151508ad931e2db566a606f29e28b81624d5173a
ittisvagyok <ittisvagyok@gmail.com>
2026-05-20 00:16:03 +0200

    Adds new steps to importing

--- web/modules/custom/euf_csv_import_export/src/CsvImporter/CsvImporter.php
+++ web/modules/custom/euf_csv_import_export/src/CsvImporter/CsvImporter.php
@@ -3,9 +3,14 @@
 namespace Drupal\euf_csv_import_export\CsvImporter;
 
 use Drupal\Core\File\FileSystem;
-use Drupal\euf_csv_import_export\FileValidator\FileValidator;
 use Drupal\euf_csv_import_export\AccessManager\UserAccessManager;
+use Drupal\euf_csv_import_export\CsvImporter\CsvSorter;
+use Drupal\euf_csv_import_export\FileValidator\FileValidator;
 use Drupal\file\Entity\File;
+use Drupal\euf_csv_import_export\CsvConverter\CsvDecoder;
+use Drupal\euf_csv_import_export\CsvConverter\FieldMappingService;
+use Drupal\euf_csv_import_export\CsvConverter\ReferenceResolver;
+use Drupal\euf_csv_import_export\Dataloader\Dataloader;
 use League\Csv\Reader;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -14,15 +19,27 @@ class CsvImporter {
 	protected FileSystem $fileSystem;
 	protected FileValidator $fileValidator;
 	protected UserAccessManager $userAccessManager;
+	protected CsvSorter $csvSorter;
+	protected CsvDecoder $csvDecoder;
+	protected ReferenceResolver $referenceResolver;
+	protected Dataloader $dataLoader;
 
 	public function __construct(
 		FileSystem $file_system,
 		FileValidator $file_validator,
 		UserAccessManager $user_access_manager,
+		CsvSorter $csv_sorter,
+		CsvDecoder $csv_decoder,
+		ReferenceResolver $reference_resolver,
+		Dataloader $data_loader,
 	)	{
 		$this->fileSystem = $file_system;
 		$this->fileValidator = $file_validator;
 		$this->userAccessManager = $user_access_manager;
+		$this->csvSorter = $csv_sorter;
+		$this->csvDecoder = $csv_decoder;
+		$this->referenceResolver = $reference_resolver;
+		$this->dataLoader = $data_loader;
 	}
 
 	public static function create(ContainerInterface $container) {
@@ -30,14 +47,38 @@ class CsvImporter {
       $container->get('file_system'),
 			$container->get('euf_csv_import_export.file_validator'),
 			$container->get('euf_csv_import_export.user_access_manager'),
+			$container->get('euf_csv_import_export.csv_sorter'),
+			$container->get('euf_csv_import_export.csv_decoder'),
+			$container->get('euf_csv_import_export.reference_resolver'),
+			$container->get('euf_csv_import_export.data_loader'),
     );
   }
 
-	public function import(File $file, string $entityType) {
+	public function import(File $file, string $entityType, ?bool $sort = FALSE) {
 		$userInstitutions = $this->userAccessManager->loadCurrentUserInstitutions();
 
 		$csvReader = $this->createReaderFromFile($file);
-		$errors = $this->fileValidator->validateFileData($csvReader, $entityType, $userInstitutions);
+		$records = iterator_to_array($csvReader->getRecords());
+    $headers = iterator_to_array($csvReader->getHeader());
+
+		$errors = $this->fileValidator->validateFileData($records, $headers, $entityType, $userInstitutions);
+		if (!empty($errors)) {
+			return ['errors' => $errors];
+		}
+
+		// Entities having parents of the same type as them, have to be sorted before import.
+		if ($sort) {
+			$records = $this->csvSorter->sortData($records, $entityType);
+			if (isset($results[CsvSorter::CYCLIC_ERROR_NAME])) {
+				return ['errors' => $records];
+			}
+		}
+
+		$results = $this->csvDecoder->decodeMultiple($records);
+
+		$institution_schac_code = $records[0][FieldMappingService::HEI_COLUMNS[$entityType]['hei_column_name']];
+		$institution_in_file = $this->dataLoader->getInstitutionBySchac($institution_schac_code);
+		$results = $this->referenceResolver->resolveReferencesMultiple($entityType, $results, $institution_in_file);
 
 		$results['errors'] = $errors;
 
